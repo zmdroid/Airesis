572f480c8d3d77b98ec6404b6614aa59d0d50851
zhou ming <393012998@qq.com>
2015-12-12 10:20:00 +0800

    update private_pub

--- script/mailman_server.rb
+++ script/mailman_server.rb
@@ -6,13 +6,13 @@ require "mailman"
 Mailman.config.logger = Logger.new("log/mailman.log")
 
 Mailman.config.pop3 = {
-    server: 'pop.alwaysdata.com', port: 110,
-    username: 'replytest@airesis.it',
-    password: 'replyalltest'
+    server: 'pop.exmail.qq.com', port: 110,
+    username: 'reply@du-kang.cn',
+    password: 'Reply123'
 }
 
 Mailman::Application.run do
-  to 'replytest+%token%@airesis.it' do
+  to 'reply+%token%@du-kang.cn' do
     begin
       ReceivedEmail.create! subject: message.subject, body: message.text_part.decoded, from: message.from.first, to: message.to.first, token: params[:token]
 
